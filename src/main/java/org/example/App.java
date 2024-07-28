0d12d939426c0fcf465abde26834486937c34923
severous <9036233+severous@user.noreply.gitee.com>
2024-07-28 22:14:33 +0800

    feat:test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,23 @@
 package org.example;
 
 
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App 
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
